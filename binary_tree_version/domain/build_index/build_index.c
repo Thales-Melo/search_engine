6c0e1e34be345572a22f8df5c42336b77171d48d
thales-penha <thales.penha@edu.ufes.br>
2023-12-13 17:04:26 -0300

    ...

--- binary_tree_version/domain/build_index/build_index.c
+++ binary_tree_version/domain/build_index/build_index.c
@@ -3,21 +3,29 @@
 #include "../../std_data/vector/vector.h"
 #include "../file_reading/dir_read.h"
 #include "../../utils/utils.h"
+#include "../collection/collection.h"
 
-int word_already_indexed () {
-    
+
+int word_already_indexed (Tree *index, char *word) {
+    return tree_contains_key(index, word);
+}
+
+int word_in_document(Tree *index, char *word, char *doc) {
+    // Se word em index
+    if (tree_contains_key(index, word)) {
+        // Se doc em colecao de documentos
+        if (tree_contains_key(((Tree*)tree_search(index, word)), doc)) {
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
 
-
-
-
-
-
-
-
-
-
+void grow_word_frequency (int *frequency) {
+    *frequency++;
+}
 
 
 
@@ -45,9 +53,11 @@ void index_build (Tree *index, Vector *files) {
             // Se a palavra já está no índice
             if (word_already_indexed(index, word)) {
                  // Se já vimos a palavra no documento
-                if (word_in_document()) {
+                if (word_in_document(index, word, vector_get(files, i))) {
                     // Incrementamos a frequência
-                    add_word_frequency();
+                    Tree* aux = (Tree*)(tree_search(index, word));
+                    Collection* col = (Collection*)tree_search(aux, vector_get(files, i));
+                    
 
                 }
                 // Caso contrário
